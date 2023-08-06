1871bc121bdff69ae800c87cf0b3de18dde1854e
ashwinimanohar <52859135+ashwinimanohar@users.noreply.github.com>
2023-08-06 19:26:42 +0930

    Update question17.sh

--- question17.sh
+++ question17.sh
@@ -1,16 +1,26 @@
-
 cd git-practice-03
+git checkout main
+mkdir dir1/dir2/foo
+mkdir dir3/bar
+mkdir dir3/bar_copy
+git add .
+git commit -m "file structure for main branch"
+git push
 
 git checkout -b branch1
-rm -r dir1/dir2/foo
+cd dir1
+mv dir2/foo foo
 rm dir3/bar_copy
-touch newfile1
+echo "hi">newfile1
 git add .
 git commit -m "Modify file structure for branch1"
+git push --set-upstream origin branch1
 
 git checkout -b branch2
 mkdir dir1/dir2
 mv dir1/foo dir1/dir2/foo_modified
 touch dir1/dir3/newfile2
+rm dir3/bar
 git add .
 git commit -m "Modify file structure for branch2"
+git push --set-upstream origin branch2
