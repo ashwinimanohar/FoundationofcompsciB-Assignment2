4e725f5101353af9cc26fbe1abf97c720ca97ec5
ashwinimanohar <52859135+ashwinimanohar@users.noreply.github.com>
2023-08-06 14:28:19 +0930

    Update question11.sh

--- question11.sh
+++ question11.sh
@@ -3,5 +3,5 @@ echo "This is file4" > file4
 git add file4
 git commit -m "Add file4 to branch2"
 echo "This is the modified content of file4" > file4
-git checkout -b branch3
+git checkout main
 git stash
