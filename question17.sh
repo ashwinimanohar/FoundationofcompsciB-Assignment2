a920eacf94767dccb6bf9faa81bcb84eb57f8ba6
ashwinimanohar <52859135+ashwinimanohar@users.noreply.github.com>
2023-08-06 22:38:08 +0930

    Update question17.sh

--- question17.sh
+++ question17.sh
@@ -23,8 +23,7 @@ git commit -m "Modify file structure for branch1"
 git push 
 
 git checkout -b branch2
-touch foo1
-mv dir3/bar dir1/dir3/newfile2
+git mv dir3/bar dir1/dir3/newfile2
 cd dir1
 mv foo dir2/foo_modified
 cd ..
